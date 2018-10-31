ccf3c0684baaaa3b836d87eca73b22ca8a38aa41
Martin Vysny <martin@vysny.me>
2018-10-31 09:05:11 +0200

    Convert AboutView to Kotlin

---
+++ src/main/java/eu/vok/bookstore/about/AboutView.kt
@@ -0,0 +1,27 @@
+package eu.vok.bookstore.about
+
+import com.github.vok.karibudsl.flow.content
+import com.github.vok.karibudsl.flow.icon
+import com.github.vok.karibudsl.flow.span
+import com.vaadin.flow.component.icon.VaadinIcon
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout
+import com.vaadin.flow.router.PageTitle
+import com.vaadin.flow.router.Route
+import com.vaadin.flow.server.Version
+import eu.vok.bookstore.MainLayout
+
+@Route(value = "About", layout = MainLayout::class)
+@PageTitle("About")
+class AboutView : HorizontalLayout() {
+    init {
+        setSizeFull()
+        content { align(center, middle) }
+
+        icon(VaadinIcon.INFO_CIRCLE)
+        span(" This application is using Vaadin Flow ${Version.getFullVersion()}.")
+    }
+
+    companion object {
+        val VIEW_NAME = "About"
+    }
+}
