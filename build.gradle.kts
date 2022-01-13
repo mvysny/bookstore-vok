20d6710e328cc55b704199a51d48e9bac5c2889a
Martin Vysny <mavi@vaadin.com>
2022-01-13 19:14:50 +0200

    dependency v-bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ val vaadinonkotlin_version = "0.9.1"
 val vaadin10_version = "14.4.9"
 
 plugins {
-    kotlin("jvm") version "1.4.31"
-    id("org.gretty") version "3.0.3"
+    kotlin("jvm") version "1.6.10"
+    id("org.gretty") version "3.0.6"
     war
     id("com.vaadin") version "0.14.3.7"
 }
@@ -52,26 +52,26 @@ dependencies {
 
     // logging
     // currently we are logging through the SLF4J API to SLF4J-Simple. See src/main/resources/simplelogger.properties file for the logger configuration
-    implementation("org.slf4j:slf4j-simple:1.7.30")
+    implementation("org.slf4j:slf4j-simple:1.7.32")
 
     // db
-    implementation("com.zaxxer:HikariCP:3.4.5")
-    implementation("org.flywaydb:flyway-core:7.1.1")
-    implementation("com.h2database:h2:1.4.200") // remove this and replace it with a database driver of your choice.
+    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("org.flywaydb:flyway-core:8.4.1")
+    implementation("com.h2database:h2:2.0.206") // remove this and replace it with a database driver of your choice.
 
     // REST
     implementation("eu.vaadinonkotlin:vok-rest:$vaadinonkotlin_version")
 
     // testing
     testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.2.12")
-    testImplementation("com.github.mvysny.dynatest:dynatest-engine:0.19")
+    testImplementation("com.github.mvysny.dynatest:dynatest-engine:0.22")
 
     // heroku app runner
-    staging("com.heroku:webapp-runner-main:9.0.41.0")
+    staging("com.heroku:webapp-runner-main:9.0.52.1")
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
 }
 
 // Heroku
