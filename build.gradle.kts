dfd07e85e62a88fef6442bc3716337ccfd02c3bc
Martin Vysny <mavi@vaadin.com>
2022-06-15 17:54:34 +0300

    dependency v-bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 // The Beverage Buddy sample project ported to Kotlin.
 // Original project: https://github.com/vaadin/beverage-starter-flow
 
-val vaadinonkotlin_version = "0.12.1"
-val vaadin_version = "23.0.6"
+val vaadinonkotlin_version = "0.13.0"
+val vaadin_version = "23.1.0"
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.7.0"
     id("org.gretty") version "3.0.6"
     war
-    id("com.vaadin") version "23.0.6"
+    id("com.vaadin") version "23.1.0"
 }
 
 defaultTasks("clean", "build")
@@ -39,28 +39,26 @@ dependencies {
     // Vaadin-on-Kotlin dependency, includes Vaadin
     implementation("eu.vaadinonkotlin:vok-framework-vokdb:$vaadinonkotlin_version")
     implementation("eu.vaadinonkotlin:vok-security:$vaadinonkotlin_version")
-    // Vaadin 14
-    implementation("com.vaadin:vaadin-core:$vaadin_version") {
-        exclude(module = "fusion-endpoint") // exclude fusion: it brings tons of dependencies (including swagger)
-    }
+    // Vaadin
+    implementation("com.vaadin:vaadin-core:$vaadin_version")
     providedCompile("javax.servlet:javax.servlet-api:3.1.0")
 
     implementation(kotlin("stdlib-jdk8"))
 
     // logging
     // currently we are logging through the SLF4J API to SLF4J-Simple. See src/main/resources/simplelogger.properties file for the logger configuration
-    implementation("org.slf4j:slf4j-simple:1.7.32")
+    implementation("org.slf4j:slf4j-simple:1.7.36")
 
     // db
     implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("org.flywaydb:flyway-core:8.4.1")
-    implementation("com.h2database:h2:2.0.206") // remove this and replace it with a database driver of your choice.
+    implementation("org.flywaydb:flyway-core:8.5.12")
+    implementation("com.h2database:h2:2.1.212") // remove this and replace it with a database driver of your choice.
 
     // REST
     implementation("eu.vaadinonkotlin:vok-rest:$vaadinonkotlin_version")
 
     // testing
-    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.3.12")
+    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.3.15")
     testImplementation("com.github.mvysny.dynatest:dynatest-engine:0.24")
 
     // heroku app runner
