a6128d6e07eb09aa62392d296fc6619506f7fd46
Martin Vysny <mavi@vaadin.com>
2023-05-14 10:21:57 +0300

    minor

--- src/main/java/eu/vok/bookstore/backend/mock/MockDataGenerator.kt
+++ src/main/java/eu/vok/bookstore/backend/mock/MockDataGenerator.kt
@@ -7,10 +7,9 @@ import eu.vok.bookstore.backend.data.Availability
 import eu.vok.bookstore.backend.data.Category
 import eu.vok.bookstore.backend.data.Product
 import eu.vok.bookstore.backend.data.ProductCategory
-import java.util.*
+import kotlin.random.Random
 
 object MockDataGenerator {
-    private val random = Random(1)
     private val categoryNames = arrayOf("Children's books", "Best sellers", "Romance", "Mystery", "Thriller", "Sci-fi", "Non-fiction", "Cookbooks")
 
     private val word1 = arrayOf("The art of", "Mastering", "The secrets of", "Avoiding", "For fun and profit: ", "How to fail at", "10 important facts about", "The ultimate guide to", "Book of", "Surviving", "Encyclopedia of", "Very much", "Learning the basics of", "The cheap way to", "Being awesome at", "The life changer:", "The Vaadin way:", "Becoming one with", "Beginners guide to", "The complete visual guide to", "The mother of all references:")
@@ -45,27 +44,19 @@ object MockDataGenerator {
         val p = Product()
         p.productName = generateName()
 
-        p.price = BigDecimal((random.nextInt(250) + 50) / 10.0)
+        p.price = BigDecimal(Random.nextDouble(5.0, 30.0))
         p.availability = Availability.values().random()
         if (p.availability == Availability.AVAILABLE) {
-            p.stockCount = random.nextInt(523)
+            p.stockCount = Random.nextInt(523)
         }
 
         p.save()
-        p.category = getCategory(categories, 1, 2)
+        p.category = getCategory(categories)
         return p
     }
 
-    private fun getCategory(categories: List<Category>,
-                            min: Int, max: Int): Set<Category> {
-        val nr = random.nextInt(max) + min
-        val productCategories = HashSet<Category>()
-        for (i in 0 until nr) {
-            productCategories.add(categories.random())
-        }
-
-        return productCategories
-    }
+    private fun getCategory(categories: List<Category>): Set<Category> =
+        (0..Random.nextInt(0, 2)).map { categories.random() } .toSet()
 
     private fun generateName(): String = "${word1.random()} ${word2.random()}"
 }
