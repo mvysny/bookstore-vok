ca96ee312c70e3b2058764c03785996628230d58
Martin Vysny <mavi@vaadin.com>
2023-09-01 16:10:37 +0300

    dep v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,8 +46,8 @@ dependencies {
 
     // db
     implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("org.flywaydb:flyway-core:9.16.0")
-    implementation("com.h2database:h2:2.2.220") // remove this and replace it with a database driver of your choice.
+    implementation("org.flywaydb:flyway-core:9.22.0")
+    implementation("com.h2database:h2:2.2.222") // remove this and replace it with a database driver of your choice.
 
     // testing
     testImplementation("com.github.mvysny.kaributesting:karibu-testing-v24:2.1.0")
