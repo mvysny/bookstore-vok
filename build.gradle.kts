eb4488a6cae6a76024a72e5316cd83a3ed3b68d4
Martin Vysny <mavi@vaadin.com>
2024-04-18 21:00:31 +0300

    dep v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.20"
-    id("application")
-    id("com.vaadin")
+    kotlin("jvm") version "1.9.23"
+    application
+    alias(libs.plugins.vaadin)
 }
 
 defaultTasks("clean", "build")
@@ -23,35 +23,33 @@ tasks.withType<Test> {
 
 dependencies {
     // Vaadin-on-Kotlin dependency, includes Vaadin
-    implementation("eu.vaadinonkotlin:vok-framework-vokdb:${project.properties["vokVersion"]}") {
+    implementation(libs.vok.db) {
         exclude(module = "vaadin-core")
     }
-    implementation("com.github.mvysny.karibudsl:karibu-dsl-v23:${project.properties["karibuDslVersion"]}")
-    implementation("com.github.mvysny.vaadin-simple-security:vaadin-simple-security:0.2")
+    implementation(libs.karibu.dsl)
+    implementation(libs.vaadin.simplesecurity)
     // Vaadin
-    implementation("com.vaadin:vaadin-core:${project.properties["vaadinVersion"]}") {
-        afterEvaluate {
-            if (vaadin.productionMode) {
-                exclude(module = "vaadin-dev")
-            }
+    implementation(libs.vaadin.core) {
+        if (vaadin.effective.productionMode.get()) {
+            exclude(module = "vaadin-dev")
         }
     }
-    implementation("com.github.mvysny.vaadin-boot:vaadin-boot:12.1")
+    implementation(libs.vaadin.boot)
 
     implementation(kotlin("stdlib-jdk8"))
 
     // logging
     // currently we are logging through the SLF4J API to SLF4J-Simple. See src/main/resources/simplelogger.properties file for the logger configuration
-    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation(libs.slf4j.simple)
 
     // db
-    implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("org.flywaydb:flyway-core:9.22.1")
-    implementation("com.h2database:h2:2.2.224") // remove this and replace it with a database driver of your choice.
+    implementation(libs.hikaricp)
+    implementation(libs.flyway)
+    implementation(libs.h2) // remove this and replace it with a database driver of your choice.
 
     // testing
-    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v24:2.1.0")
-    testImplementation("com.github.mvysny.dynatest:dynatest-engine:0.24")
+    testImplementation(libs.karibu.testing)
+    testImplementation(libs.dynatest)
 }
 
 tasks.withType<KotlinCompile> {
